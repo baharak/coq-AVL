ecd94bed77a0f3ada42180c0f8d51c3bd274dc9a
subproject <baharak1364@gmail.com>
2014-05-01 04:16:27 -0400

    new stuff for project - not clean

--- AvlTree.v
+++ AvlTree.v
@@ -14,10 +14,15 @@
 
 Require Export SfLib.
 
+Require Import Arith.Max.
+Require Import Arith.Min.
+Require Import GenericMinMax.
+
 (* Node in a graph (tree). [@node X id data] means node with id [id]
    that stores some data (properties) of type [X] *)
 Inductive node {X : Type} : Type :=
 | nd : nat -> X -> node.
+Hint Constructors node.
 
 Definition data {X : Type} (n : @node X) : X :=
   match n with
@@ -30,6 +35,7 @@ Inductive bt {X : Type} : Type :=
 | btnil : bt
 | btsub : @node X -> bt -> bt -> bt
 .
+Hint Constructors node.
 
 Check (btnil).
 Check (btsub (nd 2 _) btnil btnil).
@@ -279,7 +285,7 @@ Inductive bst {X : Type} : @bt X -> Prop :=
   (maxkey l) < k ->
   bst l ->
   r = (btsub (nd rk rd) rl rr) ->
-  (minkey r) > k ->
+  k < (minkey r) ->
   bst r ->
   bst (btsub (nd k d) l r)
 | bsthil : forall k d l lk ld ll lr,
@@ -289,7 +295,7 @@ Inductive bst {X : Type} : @bt X -> Prop :=
   bst (btsub (nd k d) l btnil)
 | bsthir : forall k d r rk rd rl rr,
   r = (btsub (nd rk rd) rl rr) ->
-  (minkey r) > k ->
+  k < (minkey r) ->
   bst r ->
   bst (btsub (nd k d) btnil r)
 .
@@ -308,22 +314,169 @@ Proof.
     constructor. (* constructor also solves inequality - no need for omega *)
 Qed.
 
-Definition hbal (h1 h2 : nat) : Prop :=
-  h1 = h2 \/ ((S h1) = h2 \/ h1 = (S h2))
+(* It is easier to work with successors of [height] because the case when
+   [height t = 0] is ambiguous - t is either [btnil] or [btsub _ btnil btnil].*)
+
+Fixpoint bthsucc {X : Type} (t : @bt X) : nat :=
+  match t with
+    | btnil => 0
+    | btsub _ l r => S (max (bthsucc l) (bthsucc r))
+  end.
+
+Lemma bthsucc_Sheight {X : Type} : forall n l r,
+  (bthsucc (@btsub X n l r)) = S (height (@btsub X n l r)).
+Proof.
+  intros. remember (btsub n l r) as t.
+  induction t. inversion Heqt.
+ (*  destruct t1; *)
+ (*    destruct t2; *)
+ (*      simpl. *)
+ (*  reflexivity.  simpl. *)
+ (*  simpl. destruct (le_either (height t1) (height t2)). *)
+ (*  rewrite max_r. *)
+ (* rewrite max_l. *)
+               (* STUCK *)
+Admitted.
+
+(* Inductive propositions [btleanl] and [btleanr] tell if the tree leans
+   to the left/right with a balance factor (height difference) of at [f].
+
+   Note that the first constructor is necessary to prove the case
+   when one child is nil (because height of both leaf and nil is 0). *)
+
+Inductive btleanl {X : Type} (f : nat) : @bt X -> Prop :=
+| btleanlnil :
+  f = 0 ->
+  btleanl f btnil
+| btleanlsub : forall n l r,
+  f + bthsucc r = bthsucc l ->
+  btleanl f (@btsub X n l r)
+.
+Hint Constructors btleanl.
+
+Inductive btleanr {X : Type} (f : nat) : @bt X -> Prop :=
+| btleanrnil :
+  f = 0 ->
+  btleanr f btnil
+| btleanrsub : forall n l r,
+  f + bthsucc l = bthsucc r ->
+  btleanr f (@btsub X n l r)
+.
+Hint Constructors btleanr.
+
+Example test_btleanr_5_nil : ~ btleanr 5 (@btnil nat).
+Proof. intro. solve by inversion 2.
+Qed.
+Example test_btleanr_0_leaf : btleanr 0 (btsub (nd 0 0) btnil btnil).
+Proof. constructor. reflexivity.
+Qed.
+Example test_btleanr_0_nil : btleanr 0 (@btnil nat).
+Proof. auto.
+Qed.
+Example test_btleanr_0_bal : btleanr 0 (btsub (nd 2 0)
+  (btsub (nd 1 0) btnil btnil)
+  (btsub (nd 3 0) btnil btnil)).
+Proof. constructor. simpl. reflexivity.
+Qed.
+Example test_btleanr_1_l : ~ btleanr 1
+(btsub (nd 2 0) (btsub (nd 1 0) btnil btnil) btnil).
+Proof. intro. solve by inversion 2.
+Qed.
+Example test_btleanr_1_r : btleanr 1
+(btsub (nd 2 0) btnil (btsub (nd 1 0) btnil btnil)).
+Proof. auto.
+Qed.
+
+Inductive ndbal {X : Type} : @bt X -> Prop :=
+| ndbalnil :
+  ndbal btnil
+| ndbalbal : forall n l r,
+  bthsucc l = bthsucc r ->
+  ndbal (btsub n l r)
+| ndbalhil : forall n l r,
+  btleanl 1 (btsub n l r) ->
+  ndbal (btsub n l r)
+| ndbalhir : forall n l r,
+  btleanr 1 (btsub n l r) ->
+  ndbal (btsub n l r)
 .
 
-Example test_hbal_bal : hbal 2 2. left. reflexivity. Qed.
-Example test_hbal_hil : hbal 1 0. right. right. reflexivity. Qed.
-Example test_hbal_hir : hbal 3 4. right. left. reflexivity. Qed.
-Example test_hbal_none_hil : ~ hbal 4 2. unfold hbal. omega. Qed.
-Example test_hbal_none_hir : ~ hbal 2 4. unfold hbal. omega. Qed.
+(*     g             p     *)
+(*    / \           / \    *)
+(*   u   p   -->   g   r   *)
+(*      / \       / \      *)
+(*     l   r     u   l     *)
+Inductive btrotl {X : Type} : @bt X -> @bt X -> Prop :=
+| btrotl0 : forall gn pn l u r, btrotl
+  (btsub gn u (btsub pn l r))
+  (btsub pn (btsub gn u l) r)
+.
 
+(*     g             p     *)
+(*    / \           / \    *)
+(*   p   u   -->   l   g   *)
+(*  / \               / \  *)
+(* l   r             r   u *)
+Inductive btrotr {X : Type} : @bt X -> @bt X -> Prop :=
+| btrotr0 : forall gn pn l u r, btrotr
+  (btsub gn (btsub pn l r) u)
+  (btsub pn l (btsub gn r u))
+.
+
+Inductive btrebal {X : Type} : @bt X -> @bt X -> Prop :=
+| btrebalbal : forall t,        (*  *)
+  ndbal t ->
+  btrebal t t
+| btreballl : forall n l r t',  (*                     g (2)         p (-1|0) *)
+  btleanl 2 (btsub n l r) ->    (*                    /             / \       *)
+  btleanl 1 l \/ btleanl 0 l -> (*                   p (1|0)  -->  l   g      *)
+  btrotr (btsub n l r) t' ->    (*                  /                         *)
+  btrebal (btsub n l r) t'      (*                 l                          *)
+| btrebalrr : forall n l r t',  (*                     g (-2)        p (1|0)  *)
+  btleanr 2 (btsub n l r) ->    (*                      \           / \       *)
+  btleanr 1 r \/ btleanr 0 r -> (*                (-1|0) p    -->  g   r      *)
+  btrotr (btsub n l r) t' ->    (*                        \                   *)
+  btrebal (btsub n l r) t'      (*                         r                  *)
+| btreballr : forall n l r l' t', (*   g (2)           g (2)         r (-1|0) *)
+  btleanl 2 (btsub n l r) -> (*       /               /             / \       *)
+  btleanr 1 l ->  (*                 p (-1)   -->    r (1|0)  -->  p   g      *)
+  btrotl l l' -> (*                   \             /                         *)
+  btrotr (btsub n l' r) t' -> (*       r           p                          *)
+  btrebal (btsub n l r) t'
+| btrebalrl : forall n l r r' t', (*   g (-2)          g (-2)        l (1|0)  *)
+  btleanl 2 (btsub n l r) -> (*         \               \           / \       *)
+  btleanl 1 r ->  (*                 (1) p    --> (-1|0) l    -->  g   p      *)
+  btrotr r r' -> (*                     /                 \                   *)
+  btrotr (btsub n l r') t' -> (*       l                   p                  *)
+  btrebal (btsub n l r) t'
+.
+
+
+(* I decided to separete the rebalancing operation from the insertion beacuse:
+   1) there would be twice as many constructors because we would repeat
+   the same rebalancing logic for both the case when I insert left and right
+   2) to avoid repeating it when I implement AVL deletion *)
+
+Inductive avlins {X : Type} : @bt X -> @node X -> @bt X -> Prop :=
+| avlinsnil : forall k' d',
+  avlins btnil (nd k' d') (btsub (nd k' d') btnil btnil)
+| avlinsleft : forall k' d' k d l r l' t,
+  k' < k ->
+  avlins l (nd k' d') l' ->
+  btrebal (btsub (nd k d) l' r) t ->
+  avlins (btsub (nd k d) l r) (nd k' d') t
+| avlinsright : forall k' d' k d l r r' t,
+  k < k' ->
+  avlins r (nd k' d') r' ->
+  btrebal (btsub (nd k d) l r') t ->
+  avlins (btsub (nd k d) l r) (nd k' d') t
+.
 
 (* AVL tree - a Binary Search Tree s.t. height diff of every subtree <= 1 *)
 Inductive avlt {X : Type} : @bt X -> Prop :=
 | avltnil : avlt btnil
 | avltsub : forall k d l r,
-  hbal (height l) (height r) ->
+  ndbal (btsub (nd k d) l r) ->
   avlt l ->
   avlt r ->
   bst (btsub (nd k d) l r) ->
@@ -333,33 +486,206 @@ Inductive avlt {X : Type} : @bt X -> Prop :=
 (* Q: Would proofs be easier if we separate bal and bst as requirements
    and define AVL tree as a combination of both (see below)? *)
 
-Inductive bal {X : Type} : @bt X -> Prop :=
-| balnil : bal btnil
+Inductive btbal {X : Type} : @bt X -> Prop :=
+| balnil : btbal btnil
 | balsub : forall k d l r,
-  hbal (height l) (height r) ->
-  bal l ->
-  bal r ->
-  bal (btsub (nd k d) l r)
+  ndbal (btsub (nd k d) l r) ->
+  btbal l ->
+  btbal r ->
+  btbal (btsub (nd k d) l r)
 .
 
-Definition avl2 {X : Type} (t : @bt X) := bst t /\ bal t.
+Definition avlt2 {X : Type} (t : @bt X) := bst t /\ btbal t.
 
-(* Q: If we add height as parameter, it becomes simpler. Is this a good idea? *)
-Inductive avl3 {X : Type} : @bt X -> nat -> Prop :=
-| avlnil' : avl3 btnil 0
-| avlbal : forall k d h l r,
-  avl3 l h ->
-  avl3 r h ->
-  bst (btsub (nd k d) l r) ->
-  avl3 (btsub (nd k d) l r) (S h)
-| avlhil : forall k d h l r,
-  avl3 l (S h) ->
-  avl3 r h ->
-  bst (btsub (nd k d) l r) ->
-  avl3 (btsub (nd k d) l r) (S (S h))
-| avlhir : forall k d h l r,
-  avl3 l h ->
-  avl3 r (S h) ->
-  bst (btsub (nd k d) l r) ->
-  avl3 (btsub (nd k d) l r) (S (S h))
-.
+
+Theorem btrebal_bst {X : Type} : forall t t',
+  @bst X t ->
+  btrebal t t' ->
+  bst t'.
+Proof.
+  intros t t' Hs Hb.
+  admit. (* TODO:  *)
+Qed.
+
+(* Lemma: A BST remains BST if we insert a smaller key to its left subtree. *)
+Lemma avlins_bst_l {X : Type} : forall k d l r k' d' l',
+  @bst X (btsub (nd k d) l r) ->
+  k' < k ->
+  avlins l (nd k' d') l' ->
+  @bst X (btsub (nd k d) l' r).
+Proof.
+Admitted.
+
+Lemma avlins_bst_r {X : Type} : forall k d l r k' d' r',
+  @bst X (btsub (nd k d) l r) ->
+  k < k' ->
+  avlins r (nd k' d') r' ->
+  @bst X (btsub (nd k d) l r').
+Proof.
+Admitted.
+
+(* Lemma: Inserting and rebalancing an AVL tree preserves its BST property. *)
+Theorem avlins_bst {X : Type} : forall t n t',
+  @bst X t -> avlins t n t' -> bst t'.
+Proof.
+  intros t n t' Hs Hi.
+  induction t;
+    inversion Hi; subst.
+  Case "insert leaf".
+    constructor.
+  Case "insert left".
+    try assert (bst (btsub (nd k d) l' t2)) as Hn
+      by (eapply avlins_bst_l; eassumption).
+    eapply btrebal_bst;
+      eassumption.
+  Case "insert right".
+    try assert (bst (btsub (nd k d) t1 r')) as Hn
+      by (eapply avlins_bst_r; eassumption).
+    eapply btrebal_bst;
+      eassumption.
+Qed.
+
+(* Lemma: After inserting to an AVL, the height increases by at most one. *)
+(* (note that it can also decrease because insertion involves rebalancing,
+   so that's we didn't put also: bthsucc t <= bthsucc t'' *)
+Lemma avlins_bthsucc {X : Type} : forall t n t'',
+  @avlins X t n t'' ->
+  bthsucc t'' <= S (bthsucc t).
+Proof.
+  intros t n t'' Ha.
+  generalize dependent t''.
+  generalize dependent n.
+  induction t; intros;
+    inversion Ha; subst;
+      simpl.
+  omega.
+
+  destruct (le_either (bthsucc t1) (bthsucc t2)).
+  rewrite max_r; try assumption.
+  apply IHt1 in H5.
+  (* TODO: need more lemmas *)
+Admitted.
+
+(* Fixpoint btleft {X : Type} (t : @bt X) : *)
+(*   match t with | *)
+(*     btnil  *)
+
+Lemma btrotr_hsucc_l {X : Type} : forall n l r n' l' r',
+  @btrotr X (btsub n l r) (btsub n' l' r') ->
+  S (bthsucc l') <= bthsucc l.
+Proof with eauto.
+  intros n0 l0 r0 n0' l0' r0' Hr.
+  inversion Hr. subst n0 l0 r0 n0' l0' r0'.
+  simpl. apply le_n_S.
+  destruct (le_either (bthsucc l) (bthsucc r)).
+  rewrite max_r; assumption.
+  rewrite max_l; omega.
+Qed.
+
+Lemma btrotr_hsucc_r {X : Type} : forall n l r n' l' r',
+  @btrotr X (btsub n l r) (btsub n' l' r') ->
+  S (bthsucc r) <= bthsucc r'.
+Proof with eauto.
+  intros n0 l0 r0 n0' l0' r0' Hr.
+  inversion Hr. subst n0 l0 r0 n0' l0' r0'.
+  simpl. apply le_n_S.
+  destruct (le_either (bthsucc r) (bthsucc u)).
+  rewrite max_r; omega.
+  rewrite max_l; assumption.
+Qed.
+
+Hint Resolve max_r.
+Hint Resolve max_l.
+Hint Resolve le_either.
+
+Lemma btrotr_bal {X : Type} : forall n l r t',
+  btleanl 2 (btsub n l r) ->
+  btleanl 1 l \/ btleanl 0 l ->
+  @btrotr X (btsub n l r) t' ->
+  ndbal t'.
+Proof with auto.
+  assert (forall n, S n = max (S n) n) as Ha.
+    intros. destruct (le_either (S n) n). omega. rewrite max_l...
+
+  intros g p u t' Hgl Hp Hr.
+  inversion Hr; subst; clear Hr.
+  destruct Hp as [Hp | Hp];
+    inversion Hp; subst; clear Hp;
+      inversion Hgl; subst; clear Hgl;
+        simpl in H0, H1.
+  Case "btleanl 1 l".
+    apply ndbalbal. simpl.
+    rewrite <- H0 in *.
+    remember (bthsucc u) as hu. clear Heqhu.
+    remember (bthsucc r) as hr. clear Heqhr.
+    erewrite <- Ha in H1.
+    destruct (max_dec hr hu); omega.
+  Case "btleanl 0 l".
+    apply ndbalhir. constructor. simpl.
+    rewrite <- H0 in *. clear H0.
+    rewrite max_idempotent in H1.
+    rewrite succ_max_distr. rewrite <- H1.
+    apply Ha.
+Qed.
+
+Theorem avlins_avlt {X : Type} : forall t n t'',
+  @avlt X t ->
+  @avlins X t n t'' ->
+  @avlt X t''.
+Proof.
+  assert (forall X t, @avlt X t -> bst t) as Has.
+    intros. inversion H; auto. constructor.
+
+  (* intros t n t'' H Hi. *)
+  (* generalize dependent Hi. *)
+  (* generalize dependent n. *)
+  (* generalize dependent t''. *)
+  (* induction H; *)
+  (*   intros. *)
+
+  (* inversion Hi. constructor. *)
+  (* apply ndbalbal. reflexivity. constructor. constructor. constructor. *)
+
+  (* inversion Hi; subst; clear Hi. *)
+
+  (* assert (bst t'') as Hs'' by *)
+  (*   (eapply btrebal_bst; try eapply avlins_bst_l; eassumption). *)
+  (* apply IHavlt1 in H10. *)
+
+
+  (* destruct t''. constructor. *)
+  (* destruct n. apply avltsub. *)
+  (* admit. inversion H10; subst. *)
+
+  intros t n t'' H Hi.
+  generalize dependent Hi.
+  generalize dependent n.
+  generalize dependent t''.
+  induction t;
+    intros.
+  admit.
+
+  inversion Hi; subst; clear Hi.
+  inversion H; subst.
+  eapply IHt1 with (t'' := l') in H8;
+    try eassumption.
+
+  assert (bst t'') as Hs'' by
+    (eapply btrebal_bst; try eapply avlins_bst_l; eassumption).
+
+  inversion H7; subst; clear H7.
+  constructor; assumption.
+  inversion H13; subst.
+  (*
+     L1 =
+ *)
+
+  admit.
+  inversion H0; subst.
+
+admit. admit. admit.
+  admit. admit.
+  (* induction H; *)
+  (*   intros. *)
+
+(* end *)
